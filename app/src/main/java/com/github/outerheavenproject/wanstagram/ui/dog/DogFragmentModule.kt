e0e0c406e45c05871d4798f87c0e40530bc0de54
Kouta Imanaka <keima@p-side.net>
2019-08-10 04:03:15 +0900

    [Not working] bind navigator
    # Conflicts:
    #	app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dog/DogFragmentModule.kt
    
    Co-authored-by: Yoshihiro Tanaka <contact@cordea.jp>

--- app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dog/DogFragmentModule.kt
+++ app/src/main/java/com/github/outerheavenproject/wanstagram/ui/dog/DogFragmentModule.kt
@@ -1,10 +1,23 @@
 package com.github.outerheavenproject.wanstagram.ui.dog
 
+import com.github.outerheavenproject.wanstagram.ui.AppNavigator
+import com.github.outerheavenproject.wanstagram.ui.AppNavigatorImpl
+import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 interface DogFragmentModule {
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(
+        modules = [
+            DogFragmentBindModule::class
+        ]
+    )
     fun contributeDogFragment(): DogFragment
+}
+
+@Module
+interface DogFragmentBindModule {
+    @Binds
+    fun bindAppNavigator(navigator: AppNavigatorImpl): AppNavigator
 }
